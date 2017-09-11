d6ec4b54cccd0d3a027ff85fa8b90e6061b001a4
vishal-krishna <santhanakrishna.k94@gmail.com>
2017-09-12 03:22:10 +0530

    User model & routes

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 mean practice 
 
 #Initial remote commit
+Setting up server with nodejs,express and db with MongoDB. created mean db.
+
+#User model & Routes
+created user model with mongoose. Using Schema. bcrypt-nodejs for encrypt password. body-parser middleware for parsing json input. presave-schema middleware to do task before saving.
--- server.js
+++ server.js
@@ -2,10 +2,18 @@ var express = require('express');
 var app = express();
 var morgan = require('morgan');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 8000;
+var User = require('./app/models/user');
 
+//for logging with color code
 app.use(morgan('dev'));
-mongoose.connect('mongodb://localhost:27017',function(err){
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+ 
+// parse application/json
+app.use(bodyParser.json());
+mongoose.connect('mongodb://localhost:27017/mean',function(err){
 	if(err){
 		console.log("not connect");
 
@@ -17,6 +25,24 @@ mongoose.connect('mongodb://localhost:27017',function(err){
 app.listen(port,function(){
 	console.log('runing on '+port);
 });
+//http://localhost:8000/home
 app.get('/home',function(req,res){
 	res.send("helo");
+});
+//http://localhost:8000/user
+app.post('/user',function(req,res){
+	var user = new User();
+	user.username = req.body.username;
+	user.password = req.body.password;
+	user.email = req.body.email;
+	if(req.body.username==null||req.body.username==''||req.body.password==null||req.body.password==''||req.body.email==null||req.body.email == ''){
+		res.send("Make sure field not empty");
+	}
+	else{
+		user.save(function(err){
+			if(err) res.send("Already exist"); 
+			res.send("usersaved");
+
+		});
+	}
 });
